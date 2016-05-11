e0307eca512f2572b265cc3f794c26aea3298bfd
the-gingerbread-man <alex.patch3@gmail.com>
2016-05-11 14:47:58 -0700

    Working on browser tests

--- test/Unit.js
+++ test/Unit.js
@@ -1,10 +1,21 @@
-const { expect, assert } = require('chai');
+// const { assert, expect, should } = require('chai');
+// const sinon = require('sinon');
 
-const { Bluetooth } = require('../lib/main.js');
+// const { Bluetooth } = require('../lib/main.js');
 
 describe('Unit Tests', function() {
-	it('Should Require Bluetooth', function() {
-		expect(Bluetooth).to.be.a('function');
-		expect(Bluetooth.prototype).to.have.property('acquire');
+	it('Bluetooth (exports object)', function() {
+		expect(Bluetooth).to.be.a('object');
+		expect(Bluetooth).to.not.be.null;
 	});
+
+	it('Bluetooth.acquire', function() {
+		expect(Bluetooth.acquire).to.be.a('function');
+
+		// Stub navigator.bluetooth.requestDevice
+		const requestDeviceStub = sinon.stub(navigator.bluetooth, 'requestDevice');
+		console.log(requestDeviceStub);
+	});
+
+
 });
